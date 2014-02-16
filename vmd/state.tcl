b46d2193ea428ade25d83e6ad07cf187b9ec19c0
Sencer Selcuk <sencerselcuk@gmail.com>
2014-02-16 14:23:14 -0500

    longer default dynamic bond length

--- vmd/state.tcl
+++ vmd/state.tcl
@@ -19,7 +19,7 @@ proc state {} {
       mol color Name
       mol selection all
       mol addrep $mid
-      mol representation DynamicBonds 2.100000 0.100000 16.000000
+      mol representation DynamicBonds 2.300000 0.100000 16.000000
       mol color Name
       mol selection all and not name H
       mol addrep $mid
