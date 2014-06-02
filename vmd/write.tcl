952c0f815b2c74f8e1b4a3e57ecb67437ad0597f
Sencer Selcuk <sencerselcuk@gmail.com>
2014-06-01 23:13:42 -0400

    if I translate the crystal wrt the cell boundaries, save it.

--- vmd/write.tcl
+++ vmd/write.tcl
@@ -1,11 +1,14 @@
 proc savexyz {} {
+  global x
+  global y
+  global z
   global celldm
   set sel [atomselect top "all"]
   set fildes [open vmd.xyz w]
   puts $fildes [$sel num]
   puts $fildes "CRYST1 $celldm"
   foreach c [$sel get {name x y z}] {
-    puts $fildes [format "%2s %14.10f %14.10f %14.10f" [lindex $c 0] [lindex $c 1] [lindex $c 2] [lindex $c 3]]
+    puts $fildes [format "%2s %14.10f %14.10f %14.10f" [lindex $c 0] [expr [lindex $c 1]-$x] [expr [lindex $c 2]-$y] [expr [lindex $c 3]-$z]]
   }
   close $fildes
   puts_red "INFO) Saved vmd.xyz file."
