2443819d578d614fe2965cf9391b65bdd721b0da
Sencer Selcuk <sencerselcuk@gmail.com>
2014-06-05 18:07:56 -0400

    let 4 char element name

--- vmd/write.tcl
+++ vmd/write.tcl
@@ -5,9 +5,9 @@ proc savexyz {sel f} {
   global celldm
   set fildes [open $f w]
   puts $fildes [$sel num]
-  puts $fildes "CRYST1 $celldm"
+  puts $fildes "CRYST1   $celldm"
   foreach c [$sel get {name x y z}] {
-    puts $fildes [format "%2s %14.10f %14.10f %14.10f" [lindex $c 0] [expr [lindex $c 1]-$x] [expr [lindex $c 2]-$y] [expr [lindex $c 3]-$z]]
+    puts $fildes [format "%-4s %14.10f %14.10f %14.10f" [lindex $c 0] [expr [lindex $c 1]-$x] [expr [lindex $c 2]-$y] [expr [lindex $c 3]-$z]]
   }
   close $fildes
   puts_red "INFO) Saved $f file."
