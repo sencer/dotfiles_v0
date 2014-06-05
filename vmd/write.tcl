978e0822e3b21061e470cfc1606ab2cdafc3ee03
Sencer Selcuk <sencerselcuk@gmail.com>
2014-06-05 18:07:23 -0400

    savexyz now expects the atom selection and filename as arguments

--- vmd/write.tcl
+++ vmd/write.tcl
@@ -1,15 +1,14 @@
-proc savexyz {} {
+proc savexyz {sel f} {
   global x
   global y
   global z
   global celldm
-  set sel [atomselect top "all"]
-  set fildes [open vmd.xyz w]
+  set fildes [open $f w]
   puts $fildes [$sel num]
   puts $fildes "CRYST1 $celldm"
   foreach c [$sel get {name x y z}] {
     puts $fildes [format "%2s %14.10f %14.10f %14.10f" [lindex $c 0] [expr [lindex $c 1]-$x] [expr [lindex $c 2]-$y] [expr [lindex $c 3]-$z]]
   }
   close $fildes
-  puts_red "INFO) Saved vmd.xyz file."
+  puts_red "INFO) Saved $f file."
 }
