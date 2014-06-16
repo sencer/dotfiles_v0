e5903b22d0a0346c2bf565a7307617dc24915267
Sencer Selcuk <sencerselcuk@gmail.com>
2014-06-16 17:48:54 -0400

    x, y, z are now arrays

--- vmd/write.tcl
+++ vmd/write.tcl
@@ -6,7 +6,12 @@ proc savexyz {sel f} {
   puts $fildes [$sel num]
   puts $fildes "celldm [pbc get -now]"
   foreach c [$sel get {name x y z}] {
-    puts $fildes [format "%-4s %14.10f %14.10f %14.10f" [lindex $c 0] [expr [lindex $c 1]-$x] [expr [lindex $c 2]-$y] [expr [lindex $c 3]-$z]]
+    puts $fildes [ \
+      format "%-4s %14.10f %14.10f %14.10f" [lindex $c 0] \
+      [expr [lindex $c 1]-$x([$sel molid])] \
+      [expr [lindex $c 2]-$y([$sel molid])] \
+      [expr [lindex $c 3]-$z([$sel molid])] \
+    ]
   }
   close $fildes
   puts_red "INFO) Saved $f file."
