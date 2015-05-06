51f69df46992843d00d651bd5883c31c42d3a8a2
Sencer Selcuk <sencerselcuk@gmail.com>
2015-05-06 15:15:27 -0400

    looks better without the {}

--- vmd/write.tcl
+++ vmd/write.tcl
@@ -2,7 +2,7 @@ proc savexyz {sel f} {
   global shift
   set fildes [open $f w]
   puts $fildes [$sel num]
-  puts $fildes "celldm [pbc get -now]"
+  puts $fildes "celldm [lindex [pbc get -now] 0]"
   foreach c [$sel get {name x y z}] {
     puts $fildes [ \
       format "%-4s %14.10f %14.10f %14.10f" [lindex $c 0] \
