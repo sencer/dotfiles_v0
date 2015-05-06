e9c0b316d84e11f794a9b66487cc7bd8e67522b7
Sencer Selcuk <sencerselcuk@gmail.com>
2015-05-06 15:15:17 -0400

    we are using shift() array, rather than x, y, z

--- vmd/write.tcl
+++ vmd/write.tcl
@@ -1,16 +1,14 @@
 proc savexyz {sel f} {
-  global x
-  global y
-  global z
+  global shift
   set fildes [open $f w]
   puts $fildes [$sel num]
   puts $fildes "celldm [pbc get -now]"
   foreach c [$sel get {name x y z}] {
     puts $fildes [ \
       format "%-4s %14.10f %14.10f %14.10f" [lindex $c 0] \
-      [expr [lindex $c 1]-$x([$sel molid])] \
-      [expr [lindex $c 2]-$y([$sel molid])] \
-      [expr [lindex $c 3]-$z([$sel molid])] \
+      [expr [lindex $c 1]-$shift([$sel molid],0)] \
+      [expr [lindex $c 2]-$shift([$sel molid],1)] \
+      [expr [lindex $c 3]-$shift([$sel molid],2)] \
     ]
   }
   close $fildes
