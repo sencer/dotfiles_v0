22ab7740b39f56d9ba7cb1e46c50a1456f991b68
Sencer Selcuk <sencerselcuk@gmail.com>
2015-05-27 22:52:22 -0400

    add functions about water:
      addwater mol_id min_distance
         solvates $mol_id with water, water comes as close as $min_distance to $mol_id
      hunwrap mol_id radius_of_H_atoms
         unwraps H atoms around periodic boundary. honors $shift array.

--- vmd/solvate.tcl
+++ vmd/solvate.tcl
@@ -1,13 +1,12 @@
-proc addwater {solute mind} {
-
+proc addwater {{solute top} {mind 2}} {
+  if { $solute=="top" } then { set solute [ molinfo top ] }
   # create a solvation box with the same size of cell
-  set ratio 0.87
-  set num [expr [lindex [molinfo list] end]+2]
-  set max {}
-  foreach r [molinfo $solute get {a b c}] {
-    lappend max [expr $r/$ratio]
-  }
-  solvate -minmax "{0 0 0} [list $max]"
+  set ratio 0.92
+  set max [vecscale [expr 1/$ratio] [molinfo $solute get {a b c}]]
+  solvate -minmax "{0 0 0} [list $max]" -o auto_add_water_solvate
+  foreach f [glob auto_add_water_solvate.*] { file delete $f }
+  set num [expr [lindex [molinfo list] end]]
+  # pbc set "$max [molinfo $solute get {alpha beta gamma}]" -molid $num
 
   # shrink it to set density ~1
   set sel [atomselect $num "name OH2"]
@@ -24,18 +23,57 @@ proc addwater {solute mind} {
     $O delete
   }
   $sel delete
-
+  # combine and remove overlapping
   set combined [TopoTools::mergemols "$solute $num"]
   molinfo $combined set {a b c alpha beta gamma} \
     [molinfo $solute get {a b c alpha beta gamma}]
   set sel [atomselect $solute all]
+  set tmp [$sel num]
   atomselect macro solute "index < [$sel num]"
   $sel delete
   set sel [atomselect $combined \
     "not (same fragment as (exwithin $mind of solute))"]
+  set tmp [expr [$sel num]-$tmp]
   $sel writepdb "merged.pdb"
   $sel delete
   mol delete $num
   mol delete [expr $num+1]
-  mol new "merged.pdb"
+  mol new "merged.pdb" waitfor all
+  return [expr $tmp/3]
+}
+
+proc hunwrap {{molid top} {rad 1}} {
+  set H [atomselect top "name H"]
+  $H set radius $rad
+  if { $molid=="top" } then { set molid [ molinfo top ] }
+  set cell [lindex [pbc get -namd] 0]
+  set inv [inverse3 [transtranspose $cell]]
+  foreach dir {0 1 0 2 0 1 0 2} {
+    topo clearbonds
+    mol bondsrecalc $molid
+    set sel [atomselect $molid "name H and numbonds 0"]
+    if {[$sel num] < 1} { break }
+    $sel delete
+    hswap $molid $dir $cell $inv
+  }
+  $H set radius 1.0
+  $H delete
+}
+
+proc hswap {molid dir mat inv} {
+  global shift
+  set s "$shift($molid,0) $shift($molid,1) $shift($molid,2)"
+  set sel [atomselect $molid "name H and numbonds 0"]
+  foreach ind [$sel get index] pos [$sel get {x y z}] {
+    set r [vecdot "[lindex $inv $dir]" [vecsub $pos $s]]
+    set atom [atomselect $molid "index $ind"]
+    # puts $r
+    if {$r < 0.20} {
+      $atom moveby [lindex $mat $dir]
+    } elseif {$r > 0.80} {
+      $atom moveby [vecscale -1 [lindex $mat $dir]]
+    }
+    $atom delete
+  }
+  $sel delete
 }
