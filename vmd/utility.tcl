4bd02470c5c622fd79c0a63450d80b93ed9a7145
Sencer Selcuk <sencerselcuk@gmail.com>
2015-05-27 22:48:46 -0400

    utility functions in utility.tcl
    inverse3 inverts a 3D matrix

---
+++ vmd/utility.tcl
@@ -0,0 +1,9 @@
+proc puts_red {string} {
+  puts stdout "\033\[31m$string\033\[0m"
+}
+
+proc inverse3 {matrix} {
+  lassign $matrix row1 row2 row3
+  set m [measure inverse "{$row1 0} {$row2 0} {$row3 0} {0 0 0 1}"]
+  return "{[lrange [lindex $m 0] 0 2]} {[lrange [lindex $m 1] 0 2]} {[lrange [lindex $m 2] 0 2]}"
+}
